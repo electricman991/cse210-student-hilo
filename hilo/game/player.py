e40228f35c0a2b7483b35c836c5cff93426c5044
Braiden Sommer <braidensommer@yahoo.com>
2021-10-08 16:18:03 -0600

    Updated player code

--- hilo/game/player.py
+++ hilo/game/player.py
@@ -1,6 +1,31 @@
-
+import random
 
 class Player:
     """A code template for a person who guesses high or low. The responsibility of this
     class of objects is to say high or low, keep track of the values, the score, and 
     determine whether or not it can throw again."""
+
+
+    def __init__(self):
+        self.points = 300
+
+    def get_points(self, wasCorrect):
+        if wasCorrect:
+            self.points += 100
+        else:
+            self.points -= 75
+
+        return self.points
+
+    def higher_or_lower(self):
+        # Returns True if a Hi was played, False if a Lo was played
+
+
+    def get_card(self):
+        # Returns a card between 1 - 13
+        return random.randrange(1,13)
+    
+    def can_play(self):
+        if self.points > 0:
+            return True
+        return False
