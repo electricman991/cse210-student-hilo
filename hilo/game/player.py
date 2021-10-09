218da0e08653cb4d69ba49da1776488f22235086
Braiden Sommer <braidensommer@yahoo.com>
2021-10-09 15:34:40 -0600

    Finished player class

--- hilo/game/player.py
+++ hilo/game/player.py
@@ -7,19 +7,36 @@ class Player:
 
 
     def __init__(self):
+        self.cards = 0
         self.points = 300
+        self.firstCard = self.get_card()
 
     def get_points(self, wasCorrect):
-        if wasCorrect:
+        if wasCorrect == 'y':
             self.points += 100
-        else:
+        elif wasCorrect == 'n':
             self.points -= 75
 
         return self.points
 
     def higher_or_lower(self):
-        # Returns True if a Hi was played, False if a Lo was played
+        first_card = self.get_card()
+        print(f"\nThe card is: {first_card}")
+        user_input = input(f'Higher or lower? [h/l] ')
+        new_card = self.get_card()
+        print(f'The new card is {new_card}')
+        if user_input == 'h' and new_card > first_card:
+            return self.get_points('y')
+        elif user_input == 'h' and new_card < first_card:
+            return self.get_points('n')
+            
+        elif user_input == 'l' and new_card < first_card:
+            return self.get_points('y')
+        else:
+            return self.get_points('n')
 
+        
+        
 
     def get_card(self):
         # Returns a card between 1 - 13
@@ -29,3 +46,7 @@ class Player:
         if self.points > 0:
             return True
         return False
+
+    
+            
+
